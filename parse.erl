8981d86c19e4924aef12b281e5d11e4a9ae7557a
wfirewood <nashuiliang@gmail.com>
2014-03-21 22:31:10 +0800

    modify parse file

--- parse.erl
+++ parse.erl
@@ -1,6 +1,5 @@
 -module(parse).
 -export([start/0]).
-%-define(IPV4_Version, 16#0800).
 
 start() ->
   {ok, File_Content} = file:read_file("first.tcpdump"),
@@ -17,37 +16,47 @@ find_ip_position(Content, N, Size) ->
       io:format("No Match~n")
   end.
 
-handle(Info) ->
-  <<Version:4, Header_Length:4, TOS:8, Total_Length: 16,
-    Identification:16, Flags:3, Fragment_Offset: 13,
+handle(Content) ->
+  <<Version:4, Header_Length:4, TOS:8, Total_Length:16,
+    Identification:16, Flags:3, Fragment_Offset:13,
     TTL:8, Protocol:8, Header_Checksum: 16,
     SourceIP1:8, SourceIP2:8, SourceIP3:8, SourceIP4:8,
-    DestinationIP1:8, DestinationIP2:8, DestinationIP3:8, DestinationIP4:8,
-    _Others/binary>> = Info,
+    DestinationIP1:8, DestinationIP2:8, DestinationIP3:8, DestinationIP4,
+  _/binary>> = Content,
 
-  {Info_Header_Checksum, _} = split_binary(Info, 20),
+  {Info_Header_Checksum, _} = split_binary(Content, 20),
 
   io:format("Number 1 (32 bits): ~n", []),
-  io:format("\tVersion: ~w~n", [Version]),
-  io:format("\tHeader length: ~w (20 bytes)~n", [Header_Length]),
-  io:format("\tTOS: ~w~n", [TOS]),
-  io:format("\tTotal length: ~w bytes~n", [Total_Length]),
+  io:format("\tVerson: ~p [~s] [4 bits]~n", [Version, version(Version)]),
+  io:format("\tHeader length: ~w [20 bytes] [4 bits]~n", [Header_Length]),
+  io:format("\tTOS: ~w [~.16X] [8 bits]~n", [TOS, TOS, "0x"]),
+  io:format("\tTotal length ~w bytes [16 bits]~n", [Total_Length]),
+
   io:format("Number 2 (32 bits): ~n", []),
-  io:format("\tIdentification: ~w (~.16X)~n", [Identification, Identification, "0x"]),
-  io:format("\tFlags: ~w (~.16X)~n", [Flags, Flags, "0x"]),
-  io:format("\tFragment offset: ~w~n", [Fragment_Offset]),
+  io:format("\tIdentification: ~w [~.16X] [16 bits]~n", [Identification, Identification, "0x"]),
+  io:format("\tFlags: ~w [~.16X] [3 bits]~n", [Flags, Flags, "0x"]),
+  io:format("\tFragment offset: ~w [~.16X] [13 bits]~n", [Fragment_Offset, Fragment_Offset, "0x"]),
+
   io:format("Number 3 (32 bits): ~n", []),
-  io:format("\tTTL(Time to live): ~w~n", [TTL]),
-  io:format("\tProtocol: ~s (~w)~n", [protocol(Protocol), Protocol]),
+  io:format("\tTTL: ~w [~.16X] [8 bits]~n", [TTL, TTL, "0x"]),
+  io:format("\tProtocol: ~w [~s] [8 bits]~n", [Protocol, protocol(Protocol)]),
 
   Sum_Header_Checksum = header_checksum(Info_Header_Checksum),
-  io:format("\tHeader checksum: ~.16X [~.16X][~s]~n", [Header_Checksum, "0x", Sum_Header_Checksum, "0x", is_correct_head_checksum(Sum_Header_Checksum)]),
-  io:format("Number 4 (32 bits): ~n", []),
-  io:format("\tSource: ~w.~w.~w.~w~n", [SourceIP1, SourceIP2, SourceIP3, SourceIP4]),
-  io:format("\tDestination: ~w.~w.~w.~w~n", [DestinationIP1, DestinationIP2, DestinationIP3, DestinationIP4]),
-  io:format("Number 5 (32 bits): ~n", []).
+  io:format("\tHeader checksum: ~.16X [~.16X][~s] [16 bits]~n",
+            [Header_Checksum, "0x", Sum_Header_Checksum, "0x", is_correct_head_checksum(Sum_Header_Checksum)]),
 
-  %io:format("~p~n", [Others]).
+  io:format("Number 4 (32 bits): ~n", []),
+  io:format("\tSource: ~w.~w.~w.~w [32 bits]~n", [SourceIP1, SourceIP2, SourceIP3, SourceIP4]),
+
+  io:format("Number 5 (32 bits): ~n", []),
+  io:format("\tDestination: ~w.~w.~w.~w [32 bits]~n", [DestinationIP1, DestinationIP2, DestinationIP3, DestinationIP4]).
+
+version(V) ->
+  case V of
+    4 -> "IPV4";
+    6 -> "IPV6"
+  end.
+
 
 is_correct_head_checksum(I) when I =:= 16#ffff -> "correct";
 is_correct_head_checksum(_) -> "incorrect".
